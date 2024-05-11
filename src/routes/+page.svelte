b88fb7c035c347cc0ba9349832bd9cddfc398933
edu <100472175@alumnos.uc3m.es>
2024-05-11 20:19:37 +0200

    added css and skeleton UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a class="underline text-red-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={() => console.log("button clicked")} class="btn-xl bg-black text-blue-400 rounded-2xl shadow-2xl"> Test </button>
+  
