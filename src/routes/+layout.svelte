69998303474cb8814a6205bc7c5c5e5b5c77f727
manu <100472310@alumnos.uc3m.es>
2024-05-13 20:33:08 +0200

    comienzo de menú de hamburguesa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,38 +1,53 @@
+<script context="module">
+	export function goto_page(page) {
+		goto(page);
+		let menu = false;
+	}
+</script>
+
 <script lang="ts">
     import {signIn, signOut} from '@auth/sveltekit/client';
 	import { goto } from '$app/navigation';
     import { page } from '$app/stores';
     import { onMount } from 'svelte';
-	import '../app.css';
+    import '../app.css';
 
 	// $page.data.session -> {user, image, etc...}
     console.log($page.data.session);
 	
 	let name = "";
 	let image = null;
+	let menu = false;
 	
     if ($page.data.session) {
         name = $page.data.session?.user?.name;
         image = $page.data.session?.user?.image;
     }
-
-	function goto_home() {
-		goto("/");
-	}
    
+   	function toggle_menu() {
+   		menu = !menu;
+   	}
 </script>
 
 <header class="bg-[#3BC4A0] grid grid-cols-5">
-	<button>Menú</button>
+	<button class="bg-white w-1/3 h-8 mt-2 ml-2 rounded-2xl" on:click={toggle_menu}>Menú</button>
 	<img class="" src="" alt="logo">
-	<button class="font-bold-italic text-white text-center py-2 text-2xl hover:underline" on:click={goto_home}>Delegación EPS</button>
+	<button class="font-bold-italic text-white text-center py-2 text-2xl hover:underline" on:click={() => {goto_page('./')}}>Delegación EPS</button>
 	{#if $page.data.session}
 		<p class="text-white italic mt-3 text-center">{name}</p>
-		<button on:click={() => signOut()} class="bg-red-500 text-white rounded-4xl h-8 mt-2 mr-2">Sign-out</button>
+		<button on:click={() => signOut()} class="bg-red-500 text-white rounded-2xl h-8 mt-2 mr-2">Sign-out</button>
 	{:else}
 		<div></div>
-		<button class="bg-white rounded-4xl h-8 mt-2 mr-2" on:click={() => signIn("google")}>Log-in</button>
+		<button class="bg-white rounded-2xl h-8 mt-2 mr-2" on:click={() => signIn("google")}>Log-in</button>
 	{/if}
 </header>
 
+{#if menu}
+	<div class="bg-[#F3F3F3] w-1/4 h-screen border-4 absolute">
+		<button class="bg-[#3BC4A0] text-center w-full h-20" on:click={() => {goto_page('./');}}><h1 class="text-2xl text-white hover:underline">Delegación EPS - Home</h1></button>
+		<button class="bg-[#3BC4A0] text-center w-full mt-1 h-20" on:click={() => {goto_page('./taquillas');}}><h1 class="text-2xl text-white hover:underline">Taquillas</h1></button>
+		<button class="bg-[#3BC4A0] text-center w-full mt-1 h-20" on:click={() => {goto_page('./');}}><h1 class="text-2xl text-white hover:underline">Osciloscopios</h1></button>
+		<button class="bg-[#3BC4A0] text-center w-full mt-1 h-20" on:click={() => {goto_page('./');}}><h1 class="text-2xl text-white hover:underline">Encuestas</h1></button>
+	</div>
+{/if}
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
-	
+	import { goto_page} from "./+layout.svelte";
 	onMount(() => {
 			//if (!$page.data.session) {
 			//	goto('/taquillas');
@@ -10,24 +10,12 @@
 			//goto('/taquillas');
 		});
 	
-	function goto_taquillas() {
-		goto('/taquillas');
-	};
-	
-	function goto_osciloscopios() {
-		goto('/osciloscopios');
-	};
-	
-	function goto_encuestas() {
-		goto('/encuestas')
-	};
-	
 </script>
 
 <div>
 	<h1 class="text-5xl text-center w-full my-8">Servicios de Delegación</h1>
 	<div class="grid-rows-3 grid place-items-center">
-		<button class="text-white text-3xl px-10 py-3 bg-[#3BC4A0] hover:bg-[#FF6D2E] rounded-2xl w-1/3 my-5 mt-10" on:click={goto_taquillas}>Taquillas</button>
+		<button class="text-white text-3xl px-10 py-3 bg-[#3BC4A0] hover:bg-[#FF6D2E] rounded-2xl w-1/3 my-5 mt-10" on:click={() => {goto_page('./taquillas');}}>Taquillas</button>
 		<button class="text-white text-3xl px-10 py-3 bg-[#3BC4A0] hover:bg-[#FF6D2E] rounded-2xl w-1/3 my-5">Osciloscopio</button>
 		<button class="text-white text-3xl px-10 py-3 bg-[#3BC4A0] hover:bg-[#FF6D2E] rounded-2xl w-1/3 my-5">Encuestas</button>
 	</div>
