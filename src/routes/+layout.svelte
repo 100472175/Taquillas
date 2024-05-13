6c33598abac185a7af6f5d5caea1a375bc21faf2
manu <100472310@alumnos.uc3m.es>
2024-05-13 13:31:01 +0200

    header started

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
     import "../app.css";
 </script>
-  
+
+<header class="bg-[#3BC4A0] grid grid-cols-5">
+	<button>Menú</button>
+	<p class="">logo</p>
+	<h1 class="font-bold-italic text-white text-center py-2 text-2xl">Delegación EPS</h1>
+	<div></div>
+	<button class="bg-white rounded-4xl h-8 mt-2 mr-2">Log-in</button>
+</header>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
-
-<h1>Welcome to SvelteKit</h1>
+<div>
+	<h1 class="text-5xl text-center w-full my-8">Servicios de la Delegación</h1>
+	<div class="grid-cols-max">
+		<button class="text-green-500 text-3xl px-10 py-3 bg-black rounded-2xl">Taquillas</button>
+		<button class="text-green-500 text-3xl px-10 py-3 bg-black rounded-2xl">Osciloscopio</button>
+		<button class="text-green-500 text-3xl px-10 py-3 bg-black rounded-2xl">Encuestas</button>
+	</div>
+</div>
 <p>Visit <a class="underline text-red-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button on:click={() => console.log("button clicked")} class="btn-xl bg-black text-blue-400 rounded-2xl shadow-2xl"> Test </button>
   
