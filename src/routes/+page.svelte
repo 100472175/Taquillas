532250556d271a2cabd4264dbbdc7f86b672d603
manu <100472310@alumnos.uc3m.es>
2024-05-15 19:03:19 +0200

    acordeón arrgelado

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,25 +22,21 @@
 <div>
 	<h1 class="text-5xl text-center w-full my-8">Servicios de Delegación</h1>
 	<div class="grid-rows-3 grid place-items-center">
-		<Accordion class="w-1/3 mb-2">
-			<AccordionItem class="text-white text-3xl px-8 py-3 bg-[#3BC4A0] hover:bg-[#FF6D2E] rounded-2xl">	
+		<Accordion class="w-1/3">
+			<AccordionItem class="text-white text-3xl px-8 py-3 bg-[#3BC4A0] mb-2 hover:bg-[#FF6D2E] rounded-2xl">	
 				<button slot="header" class="flex gap-2" on:click={() => { goto('./taquillas'); }}>
 					<ChartPieSolid class="mt-2"/>
 					Taquillas
 				</button>
 				<p>Reserva o comprueba el estado de una taquilla</p>
 			</AccordionItem>
-		</Accordion>
-		<Accordion class="w-1/3">
-			<AccordionItem class="text-white text-3xl px-8 py-3 bg-[#3BC4A0] hover:bg-[#FF6D2E] rounded-2xl">	
+			<AccordionItem class="text-white text-3xl px-8 py-3 bg-[#3BC4A0] mb-2 hover:bg-[#FF6D2E] rounded-2xl">	
 				<button slot="header" class="flex gap-2" on:click={() => { goto('./osciloscopio'); }}>
 					<DrawSquareOutline class="mt-2"/>
 					Osciloscopio
 				</button>
 				<p>Reserva un osciloscopio en el despacho</p>
 			</AccordionItem>
-		</Accordion>
-		<Accordion class="w-1/3">
 			<AccordionItem class="text-white text-3xl px-8 py-3 bg-[#3BC4A0] hover:bg-[#FF6D2E] rounded-2xl">	
 				<button slot="header" class="flex gap-2" on:click={() => {goto('./encuestas');}}>
 					<AnnotationSolid class="mt-2"/>
