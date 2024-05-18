d5e1ac0481def4acfd075f6411b017656fccf988
manu <100472310@alumnos.uc3m.es>
2024-05-18 22:27:29 +0200

    añadida pequeña página de info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,8 +19,8 @@
 
 <Konami />
 
-<div>
-	<h1 class="text-5xl text-center w-full my-8">Servicios de Delegación</h1>
+<div class="h-fit">
+	<h1 class="text-5xl text-center w-full my-6">Servicios de Delegación</h1>
 	<div class="grid-rows-3 grid place-items-center">
 		<Accordion class="w-1/3">
 			<AccordionItem class="text-white text-3xl px-8 py-3 bg-[#3BC4A0] mb-2 hover:bg-[#FF6D2E] rounded-2xl">	
@@ -46,4 +46,16 @@
 			</AccordionItem>
 		</Accordion>
 	</div>
+</div>
+<div class="bg-[#3BC4A0] w-full h-screen flex items-center justify-center gap-24">
+	<div class="rounded-full bg-white flex items-center justify-center text-center aspect-square w-5/12 border-solid border-4">
+		<p class="flex items-center justify-center text-3xl">
+			Delegación gestiona las taquillas de la universidad. Si quieres alquilar una, simplemente tendrás que elegir la que quieres y luego rellenar un formulario para reservarla.
+		</p>
+	</div>
+	<button class="text-white text-3xl px-8 py-3 bg-[#FF6D2E] rounded-2xl w-1/3 flex gap-2"
+			on:click={() => {goto('./taquillas');}}>
+		<LockOpenOutline class="mt-2 w-1/4"/> 
+		<p class="text-left w-full">Ir a Taquillas</p>
+	</button>
 </div>
