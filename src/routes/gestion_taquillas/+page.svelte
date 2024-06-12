aae0ba87333267ebe2408c407aa23195ee89cf42
Eduardo <100472175@alumnos.uc3m.es>
2024-06-12 23:33:09 +0200

    Fix para limpiar el form al cambiar de focus tab

--- src/routes/gestion_taquillas/+page.svelte
+++ src/routes/gestion_taquillas/+page.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
+	/** @type {import('./$types').ActionData} */
+	export let form;
+
 	import { enhance } from '$app/forms';
 	import { page } from '$app/stores';
-	import { Tabs, TabItem, Input, Label, Button, Select, Toast, Card } from 'flowbite-svelte';
+	import { Tabs, TabItem, Input, Label, Button, Modal, Card } from 'flowbite-svelte';
 	let session;
 
 	// Reactive statement to update session whenever $page.data.session changes
 	$: session = $page.data.session;
 
-	/** @type {import('./$types').ActionData} */
-	export let form;
-	let hecho = false;
+	let formModalReservation = false;
+	let selectedTaquilla = '';
 </script>
 
 <h1 class="text-4xl text-center text-[#3BC4A0] m-5">Gestión de Taquillas</h1>
@@ -19,6 +21,9 @@
 		title="Búsqueda por NIA"
 		class="hover:text-[#3BC4A0]"
 		inactiveClasses="text-gray-500 hover:text-[#3BC4A0] p-4"
+		on:focus={() => {
+			form = null;
+		}}
 	>
 		<form action="?/busquedaNia" method="post" use:enhance>
 			<div class="grid grid-cols-1">
@@ -46,6 +51,9 @@
 		title="Búsqueda por Taquilla"
 		class="hover:text-[#3BC4A0]"
 		inactiveClasses="text-gray-500 hover:text-[#3BC4A0] p-4"
+		on:focus={() => {
+			form = null;
+		}}
 	>
 		<form class="w-screen" action="?/busquedaTaquilla" method="post" use:enhance>
 			<div class="grid grid-cols-1">
@@ -71,26 +79,65 @@
 	</TabItem>
 </Tabs>
 
-<div class="w-screen grid grid-cols-1 place-items-center mt-2">
-	{#if form}
-		
+<div class="w-screen grid grid-cols-2 place-items-center mt-2">
+	{#if form != null && form}
 		{#each form.reservas as taquilla}
 			<Card class="mt-2">
 				<div class="grid grid-cols-2">
 					<h5 class="text-2xl text-[#3BC4A0]">{taquilla['taquilla']}</h5>
 					<p class="text-right pr-10 p-1 text-black">{taquilla['status']}</p>
 				</div>
-				<p class="text-black text-sm">{taquilla['date']}</p>
+				<p class="text-black text-sm">Reservada el {taquilla['date']}</p>
 				{#if taquilla['status'] === 'reservada'}
 					<div class="grid grid-cols-2 mt-4 place-items-center">
 						<button class="w-2/3 text-white bg-green-500 rounded">Confirmar</button>
 						<button class="w-2/3 text-white bg-red-500 rounded">Eliminar</button>
+					</div>
+				{:else if taquilla['status'] === 'libre'}
+					<div class="grid grid-cols-2 mt-4 place-items-center">
+						<button
+							class="w-2/3 text-white bg-green-500 rounded"
+							on:click={() => {
+								selectedTaquilla = taquilla['taquilla'];
+								formModalReservation = true;
+							}}
+						>
+							Reservar
+						</button>
 					</div>
 				{/if}
 			</Card>
 		{/each}
 	{/if}
 </div>
+
+<Modal bind:open={formModalReservation} size="xs" autoclose={false} class="w-full">
+	<form class="flex flex-col space-y-6" action="?/registerTaquilla" method="post">
+		<h3 class="mb-2 text-xl font-medium text-gray-900 dark:text-white">Reservar Taquilla</h3>
+		<p>
+			Vas a realizar la reserva de una taquilla. El precio de las taquilla es de:
+			{#if selectedTaquilla.includes('G')}<span class="font-bold"> 6€ </span>
+			{:else}<span class="font-bold"> 4€ </span>
+			{/if} el año completo y la mitad por el segundo cuatrimestre. Este importe se abona en delegación
+			de estudiantes.
+		</p>
+		<Label class="space-y-2">
+			<span>NIA:</span>
+			<Input type="text" id="nia" name="nia" placeholder="100xxxxxx" required />
+		</Label>
+		<Label class="space-y-2">
+			<span>Taquilla</span>
+			<Input type="text" id="taquilla" name="taquilla" value={selectedTaquilla} readonly required />
+		</Label>
+
+		<Label>
+			<span>Nombre:</span>
+			<Input type="text" id="nombre" name="nombre" required />
+		</Label>
+
+		<Button type="submit" class="w-full1 bg-green-500 hover:bg-blue-400">Reservar Taquilla</Button>
+	</form>
+</Modal>
 
 <p>Comentarios:</p>
 Estaría bien que fuera un desplegable de NIAS para borrar los roles. También tenemos que hacer una página
